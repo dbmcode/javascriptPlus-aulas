f200e86b73129441f1cf0ffd6a8799e272280571
Marcos <marcoslopes.dev@gmail.com>
2023-08-17 10:43:14 -0300

    aula variáveis

---
+++ variáveis/script.js
@@ -0,0 +1,31 @@
+//3  formas de criar variáveis;
+
+//antes do ES6 2015.
+// var carro = "sua cor é preto";
+// // depois do ES6 2015.
+// let moto = "é muito rápida";
+// const casa = "é amarela";
+
+// console.log(carro, moto);
+
+//escopo
+//mudança
+
+var a = 5;
+let b = 6;
+
+if (true) {
+  b = 4;
+  console.log(b);
+}
+
+console.log(b);
+
+let _carro = "preto";
+let $moto = "rápida";
+let Carro = "azul";
+let carr78 = "grande";
+
+console.log(carro);
+console.log($moto);
+console.log(Carro);
