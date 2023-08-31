d4090862b7ebbead5debcf688d8f29bff0993b34
Marcos <marcoslopes.dev@gmail.com>
2023-08-31 16:27:51 -0300

    feat: desafio

---
+++ 1-variáveis/DESAFIO.md
@@ -0,0 +1,5 @@
+# Desafio
+
+Faça um programa que armazena na memória o `nome` e `sobrenome`. Deve armazenar na memória a frase: 'Olá, seja bem-vindo'. Depois, faça a exibição do resultado no console, utilizando a função console.log(). O resultado esperado deve ser: 'Olá, seja bem-vindo Marcos Lopes'."
+
+**"Ola seja bem-vindo Marcos lopes"**
--- 1-variáveis/script.js
+++ 1-variáveis/script.js
@@ -1,31 +1,29 @@
 //3  formas de criar variáveis;
 
-//antes do ES6 2015.
-// var carro = "sua cor é preto";
-// // depois do ES6 2015.
-// let moto = "é muito rápida";
-// const casa = "é amarela";
+// //antes do ES6 2015.
+// // var carro = "sua cor é preto";
+// // // depois do ES6 2015.
+// // let moto = "é muito rápida";
+// // const casa = "é amarela";
 
-// console.log(carro, moto);
+// // console.log(carro, moto);
 
-//escopo
-//mudança
+// //escopo
+// //mudança
 
-var a = 5;
-let b = 6;
+// var a = 5;
+// let b = 6;
 
-if (true) {
-  b = 4;
-  console.log(b);
-}
+// if (true) {
+//   b = 4;
+//   console.log(b);
+// }
 
-console.log(b);
+// console.log(b);
 
-let _carro = "preto";
-let $moto = "rápida";
+let carro = "preto";
+let moto = "rápida";
 let Carro = "azul";
 let carr78 = "grande";
 
-console.log(carro);
-console.log($moto);
-console.log(Carro);
+console.log(carro + moto);
