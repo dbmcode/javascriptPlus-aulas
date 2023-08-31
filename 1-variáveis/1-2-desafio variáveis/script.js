67999b12ff1c2f2e347059a9cebf19b70da1b627
Marcos <marcoslopes.dev@gmail.com>
2023-08-31 18:27:35 -0300

    comentário e camelcase

---
+++ 1-variáveis/1-2-desafio variáveis/script.js
@@ -0,0 +1,6 @@
+let carro = "preto";
+let moto = "rápida";
+let Carro = "azul";
+let carr78 = "grande";
+
+console.log(carro + moto);
---
+++ 1-variáveis/1-3-comentarios/script.js
@@ -0,0 +1,5 @@
+const carroVermelho = 2000;
+
+const nomeCompletoFuncionario = "Marcos lopes juvencio";
+
+console.log(nomeCompletoFuncionario);
