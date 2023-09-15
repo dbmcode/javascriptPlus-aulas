44169fea2927121924e5b6c5babb124fce049a40
Marcos <marcoslopes.dev@gmasil.com>
2023-09-15 14:13:31 -0300

    add : nul e undefined

---
+++ 2-tipos-de-dados/2-4-null-undefined/script.js
@@ -0,0 +1,20 @@
+//Null
+//Undefined
+
+undefined; // nao definido
+
+null; // nao existencia, ou seja não existe
+
+// let user = undefined;
+
+// if (!user) {
+//   user = "marcos";
+// }
+
+// console.log(user);
+
+undefined; //indica que uma variável foi criada mas nao foi inicializado com valor
+
+// let cor;
+
+let cor = null;
