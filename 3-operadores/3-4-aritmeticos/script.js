adaeb4e3515b48e446e313785cb6c4385f5a571f
Marcos <marcoslopes.dev@gmasil.com>
2023-09-18 10:56:44 -0300

    add: aula aritmeticos II

---
+++ 3-operadores/3-4-aritmeticos/script.js
@@ -0,0 +1,48 @@
+/*   
+OPERADOR PADRÃO
+
++  soma
+-  subtração
+*  mutiplicação
+/  divisão
+%  modulo
+*/
+
+//SOMA
+2 + 3;
+//SUBTRAÇÃO
+10 - 5;
+
+//divisão
+const divisao = 10 / 5;
+
+//mulltiplicação
+10 * 5;
+
+// módulo
+
+const modulo = 12 % 5;
+
+console.log(divisao);
+console.log(modulo);
+
+/*
+COMPLEMENTOS
+++  incremento
+--  decremento
+*/
+
+// -  negação
+//+  adição
+
+let x = 0;
+let y = 0;
+
+//   x = x + 1
+//prefixado
+
+console.log(++x);
+
+//posfixado
+
+console.log(y++);
