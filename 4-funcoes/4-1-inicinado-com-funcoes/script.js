47f62743cb453b7c9cbef2e31c00a52517e6445b
Marcos <marcoslopes.dev@gmasil.com>
2023-09-18 09:39:00 -0300

    add:  aula mod 4

---
+++ 4-funcoes/4-1-inicinado-com-funcoes/script.js
@@ -0,0 +1,7 @@
+//Funções - servem para
+
+function somaDoisNumero(a, b) {
+  console.log(a + b);
+}
+
+somaDoisNumero(3, 4);
---
+++ 4-funcoes/4-2 escopo e return/script.js
@@ -0,0 +1,10 @@
+const texto1 = "fui nomercado";
+const texto2 = "comprei macarrão";
+
+function concatenaMostrar(a, b) {
+  const result = `${a} ${b} faltou dinheiro`;
+  return;
+  console.log(result);
+}
+
+const result = concatenaMostrar(texto1, texto2);
