a13d2ecb3b8fe541bd89d9ae2423d04cc50a73dc
Marcos <marcoslopes.dev@gmasil.com>
2023-09-18 14:46:35 -0300

    add: aula

---
+++ 4-funcoes/4-3-arrow-function/script.js
@@ -0,0 +1,18 @@
+//Arrow function
+// const total = soma(4, 5);
+
+// const soma = (a, b) => {
+//   return a + b;
+// };
+
+// console.log(total);
+
+// const total = soma(4,5)
+
+// function soma(a,b){
+//     return a+b;
+// }
+
+// console.log(total)
+
+//hoisted - ok
