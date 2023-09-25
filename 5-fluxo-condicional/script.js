e42d88d4f4dc324c8f110835ddb4a44a960b101b
Marcos <marcoslopes.dev@gmail.com>
2023-09-25 16:49:59 -0300

    feat: fluxo condicional

---
+++ 5-fluxo-condicional/script.js
@@ -0,0 +1,27 @@
+// declaração IF
+
+// let nome = "lopes";
+
+// if (nome != "Claudio") {
+//   nome = "Claudio-> alterado";
+// }
+
+// console.log(`O nome é ${nome}`);
+
+// if (nome != "marcos") console.log(nome);
+
+let nome = "carlos";
+
+// if (nome == "Roberto") {
+//   console.log("ola Robeto");
+// } else if (nome == "carlos") {
+//   console.log("ola carlos");
+// } else if (nome == "Jose") {
+//   console.log("ola jose");
+// } else {
+//   console.log("não atendeu a solicitação");
+// }
+
+if ((nome && nome == "carlos") || (nome == "janeiro" && !idade)) {
+  console.log("ok confirmado");
+}
